0c5175138f2806bc9d030fc457de966abf129f9d
Tokiarivelo <tokiarivelo@hairun-technology.com>
2023-10-17 16:52:41 +0300

    initia commit

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  extends: ["@nuxtjs/eslint-config-typescript", "plugin:prettier/recommended"],
+  rules: {
+    // override/add rules settings here, such as:
+    // 'vue/no-unused-vars': 'error'
+  },
+};
---
+++ app.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppAlert> This is an auto-imported component. </AppAlert>
+  </div>
+</template>
+<script setup lang="ts">
+// ------------------ runtime config / .env ---------------
+const runtimeConfig = useRuntimeConfig();
+console.log(
+  "runtimeConfig.apiSecretConfig :>> ",
+  runtimeConfig.apiSecretConfig,
+);
+console.log("runtimeConfig.public.apiBase :>> ", runtimeConfig.public.apiBase);
+console.log(
+  "runtimeConfig.public.apiBase :>> ",
+  runtimeConfig.public.apisecret,
+);
+
+// ------------- App config ------------------
+const appConfig = useAppConfig();
+
+console.log("appConfig.title :>> ", appConfig.title);
+console.log("appConfig.theme :>> ", appConfig.theme);
+</script>
